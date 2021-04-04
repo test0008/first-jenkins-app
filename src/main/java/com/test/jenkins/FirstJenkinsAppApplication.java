ec23012013f89f9c96a395c8aa2e66aae40c2fd0
test0008 <er.javatest@gmail.com>
2021-04-04 23:38:52 +0530

    my first commit

---
+++ src/main/java/com/test/jenkins/FirstJenkinsAppApplication.java
@@ -0,0 +1,13 @@
+package com.test.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJenkinsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJenkinsAppApplication.class, args);
+	}
+
+}
